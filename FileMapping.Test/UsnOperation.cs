3d8aca672241968b36569cefb57a7d3e6b9a2b27
Tritium Qin <tritium404@hotmail.com>
2024-02-18 22:03:04 +0800

    [测试]创建非法卷句柄测试

--- FileMapping.Test/UsnOperation.cs
+++ FileMapping.Test/UsnOperation.cs
@@ -6,9 +6,17 @@ namespace FileMapping.Test;
 [TestClass]
 public class UsnOperation
 {
+	[DataRow('A')]
+	[DataRow('B')]
+	[TestMethod]
+	public void InvalidVolumeHandle(char diskLetter)
+	{
+		var hDevice = UsnUtils.GetVolumeHandle(diskLetter);
+		Assert.IsNull(hDevice, "使用非法盘符获取的句柄竟然非空");
+	}
 	[DataRow('C')]
 	[TestMethod]
-	public void GlobalTest(char diskLetter)
+	public void CreateReadAndDeleteUsnJournal(char diskLetter)
 	{
 		var (volumeName, fileSystemName, serialNumber, fileSystemFlags) = UsnUtils.GetVolumeInfo(diskLetter);
 		Trace.WriteLine($"{serialNumber} {volumeName} {fileSystemName} {fileSystemFlags}");
