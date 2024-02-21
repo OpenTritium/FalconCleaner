5455e68a8930ac03b315db96eb61d0d0ba6b49d3
Tritium Qin <tritium404@hotmail.com>
2024-02-21 11:44:00 +0800

    新建命令行项目

---
+++ Falcon CLI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using ABI.Windows.Web.UI;
+using FileMapping.RawDiskProber;
+
+var deviceHandle = RawDiskUtils.GetPhysicalDiskHandle(1);
+if (deviceHandle is null)
+{
+	Console.WriteLine("物理磁盘句柄无效");
+	return;
+}
+Console.WriteLine("设备句柄有效");
+if (!RawDiskUtils.ShiftPointerForward(deviceHandle, 0, out var newOne))
+{
+	Console.WriteLine("移动句柄失败");
+	return;
+}
+Console.WriteLine("移动句柄成功");
+
+if (RawDiskUtils.ExploitBytes(deviceHandle, 512, Parser))
+{
+	Console.WriteLine("正常结束");
+	return;
+}
+Console.WriteLine("不正常");
+return;
+
+static bool Parser(byte[] buffer, uint bytesCount)
+{
+	Console.WriteLine(buffer[510]);
+	return true;
+}
