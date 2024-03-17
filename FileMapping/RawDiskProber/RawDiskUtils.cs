ab5349c95fc7c63784553e0337caa521ea5f72d2
Tritium Qin <tritium404@hotmail.com>
2024-03-18 00:08:37 +0800

    新增关于移动文件指针的说明

--- FileMapping/RawDiskProber/RawDiskUtils.cs
+++ FileMapping/RawDiskProber/RawDiskUtils.cs
@@ -23,7 +23,15 @@ public static class RawDiskUtils
 		);
 		return physicalDiskHandle.IsInvalid ? null : physicalDiskHandle;
 	}
-
+	/*
+	 * 需要注意的是，此函数返回的文件指针不适用于重叠读写操作。
+	 * 对于重叠操作的偏移量，请使用 OVERLAPPED 结构的 Offset 和 OffsetHigh 成员。
+	 * 此外，不能将 SetFilePointerEx 用于非寻址设备（如管道或通信设备）的句柄。
+	 * 在多线程应用程序中设置文件指针时要小心，必须同步对共享资源的访问。
+	 * 例如，如果应用程序的线程共享一个文件句柄、更新文件指针并从文件中读取数据，
+	 * 应该使用临界区对象或互斥对象来保护这个序列。关于这些对象的更多信息，请参阅临界区对象和互斥对象1.
+	 */
+	// 偏移后的指针不需要释放
 	public static bool ShiftPointerForward(SafeFileHandle currentPointer, long distance,
 		out IntPtr newPointer)
 	{
